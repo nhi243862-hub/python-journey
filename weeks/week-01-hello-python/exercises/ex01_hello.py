30ab03d2bde2e5bfc2ee13162a44daab5ad1d08e
Tuyet Nhi <nhi243862@gmail.com>
2026-05-18 19:00:36 +0700

    TuyetNhi

--- weeks/week-01-hello-python/exercises/ex01_hello.py
+++ weeks/week-01-hello-python/exercises/ex01_hello.py
@@ -8,38 +8,32 @@ Chạy file này bằng: python ex01_hello.py
 """
 
 # TODO 1: In ra dòng chữ "Hello, World!"
-# Viết code ở đây:
+print("Hello, World!")
 
 
 # TODO 2: In ra tên của bạn trên một dòng riêng
-# Viết code ở đây:
+print("Nguyễn Thị Tuyết Nhi")
 
 
 # TODO 3: In ra 3 điều bạn thích, mỗi điều trên một dòng
-# Ví dụ:
-# Tôi thích ăn phở
-# Tôi thích nghe nhạc
-# Tôi thích code Python
-# Viết code ở đây:
+print("Tôi thích ăn phở")
+print("Tôi thích nghe nhạc")
+print("Tôi thích code Python")
 
 
 # TODO 4: In ra một hình chữ nhật bằng dấu *
-# Kết quả mong muốn:
-# *****
-# *   *
-# *   *
-# *****
-# Viết code ở đây:
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
 
 
 # TODO 5 (Thử thách): In ra tên bạn bằng ký tự đặc biệt
-# Ví dụ nếu tên là "HI":
-# H   H  III
-# H   H   I
-# HHHHH   I
-# H   H   I
-# H   H  III
-# Viết code ở đây:
+print("N   N  H   H  I")
+print("NN  N  H   H  I")
+print("N N N  HHHHH  I")
+print("N  NN  H   H  I")
+print("N   N  H   H  I")
 
 
 print("\n🎉 Chúc mừng! Bạn đã hoàn thành bài tập đầu tiên!")
